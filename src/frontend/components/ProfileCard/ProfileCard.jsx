fe1a6085c3e6a64c208100f5694b7a7387c89232
Harshit Anand <harshitanand97@gmail.com>
2022-05-17 01:11:48 +0530

    Create ProfileCard.jsx

---
+++ src/frontend/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { User } from '../User/User'
+import ProfileCardCSS from "./ProfileCard.module.css"
+import linkedin from "../../assets/linkedin.png"
+import github from "../../assets/github.png"
+import twitter from "../../assets/twitter.png"
+// import { PostCard } from '../PostCard/PostCard'
+
+function ProfileCard() {
+  return (
+    <div className={ProfileCardCSS.container}>
+        <div className={`d-flex ${ProfileCardCSS.header}`}>
+            <User/>
+            <span>Edit</span>
+        </div>
+        <article className={ProfileCardCSS.description}>
+            Aspiring Front-End Developer
+        </article>
+        <ul className={`d-flex ${ProfileCardCSS.stats}`}>
+            <li>3 Posts</li>
+            <li>5 Followers</li>
+            <li>2 Following</li>
+        </ul>
+        <a href="https://www.linkedin.com/in/hanand2021" target={"_blank"} className={ProfileCardCSS.social_media}>
+            <img src={linkedin} alt="linkedin icon" />
+            <img src={github} alt="github icon" />
+            <img src={twitter} alt="twitter icon" />
+        </a>
+    </div>
+  )
+}
+
+export {ProfileCard}
