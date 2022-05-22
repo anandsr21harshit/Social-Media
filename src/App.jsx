0572dd4ed1eea144a0b34ebc7f214d91768e7556
Harshit Anand <harshitanand97@gmail.com>
2022-05-22 12:44:26 +0530

    Update: Routes are  protected

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,20 @@ import {Route, Routes} from "react-router-dom"
 import MockAPI from "./frontend/components/Mockman/Mock";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import { useSelector } from "react-redux";
+import RequireAuth from "./frontend/features/auth/RequireAuth";
 
 function App() {
+  const {token} = useSelector(state => state.auth);
+
   return (
     <>
       <ToastContainer position="top-right" autoClose={1000}/>
       <Header/>
       <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/home" element={<RequireAuth token={token}><Home/></RequireAuth>}/>
+        <Route path="/profile" element={<RequireAuth token={token}><Profile/></RequireAuth>}/>
         <Route path="/mock-api" element={<MockAPI/>}/>
      </Routes>
      <NavBar/>
