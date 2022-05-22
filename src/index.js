fb820a00272920be169e48df6471522644c9f1ad
Harshit Anand <harshitanand97@gmail.com>
2022-05-22 12:26:49 +0530

    Update: added Router and Provider

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-import {BrowserRouter as Router} from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import {store} from "./frontend/app/store"
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
