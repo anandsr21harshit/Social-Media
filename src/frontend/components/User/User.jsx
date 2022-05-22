f5ffb1f7f3c93aa63da9d8c6354c2e16d6190fd2
Harshit Anand <harshitanand97@gmail.com>
2022-05-22 12:25:37 +0530

    Feat: dynamic user details

--- src/frontend/components/User/User.jsx
+++ src/frontend/components/User/User.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import UserCSS from "./User.module.css"
 import man from "../../assets/man.png"
+import { useSelector } from "react-redux";
 
 
 function User() {
+  const {user} = useSelector(state => state.auth);
+
   return (
     <div className={`d-flex ${UserCSS.container}`}>
       <img src={man} alt="Iser Icon" />
-      <p>User Name</p>
+      <h4>{user?.firstName} {user?.lastName}</h4>
+      <span className="username">@{user?.username}</span>
     </div>
   );
 }
