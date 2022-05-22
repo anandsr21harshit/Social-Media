e35ca2367615a05e7ed5c99ba31e704c085527df
Harshit Anand <harshitanand97@gmail.com>
2022-05-22 12:22:43 +0530

    Update: ToastContainer and MockApi added

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import { Home } from "./frontend/pages/home/Home";
 import { Login } from "./frontend/pages/login/Login";
 import { Profile } from "./frontend/pages/profile/Profile";
 import {Route, Routes} from "react-router-dom"
+import MockAPI from "./frontend/components/Mockman/Mock";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <>
+      <ToastContainer position="top-right" autoClose={1000}/>
       <Header/>
       <Routes>
         <Route path="/" element={<Login/>}/>
         <Route path="/home" element={<Home/>}/>
         <Route path="/profile" element={<Profile/>}/>
+        <Route path="/mock-api" element={<MockAPI/>}/>
      </Routes>
      <NavBar/>
     </>
