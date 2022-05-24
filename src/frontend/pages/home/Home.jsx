b07e2a89e30a00dcca10f5a66c4b520fb6f50df3
Harshit Anand <harshitanand97@gmail.com>
2022-05-24 10:19:49 +0530

    Update: fetching data from db

--- src/frontend/pages/home/Home.jsx
+++ src/frontend/pages/home/Home.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NewPost } from '../../components/NewPost/NewPost'
 import { PostCard } from '../../components/PostCard/PostCard'
 import HomeCSS from  "./Home.module.css"
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllPosts } from '../../features/post/postSlice'
+import { getAllUsers } from '../../features/user/userSlice'
 
 function Home() {
+  const {allPosts} = useSelector(state => state.posts);
+  const {allUsers} = useSelector( state => state.users);
+
+  // sort allPosts date wise
+  // use spread operator to stop mutation
+  const feePosts = [...allPosts].sort((a,b) => new Date(b.createdAt) - new Date(a.createdAt))
+
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(getAllPosts())
+    dispatch(getAllUsers())
+  },[])
+
   return (
     <section className={`d-flex ${HomeCSS.container}`}>
         <NewPost/>
-        <PostCard/>
+        {feePosts.map(post => <PostCard {...post} key={post._id}/> )}
     </section>
   )
 }
