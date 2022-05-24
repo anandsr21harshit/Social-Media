2b037acc23d57cee572c2bec8d65974b003b7b85
Harshit Anand <harshitanand97@gmail.com>
2022-05-24 10:21:42 +0530

    Update PostCard.jsx

--- src/frontend/components/PostCard/PostCard.jsx
+++ src/frontend/components/PostCard/PostCard.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import { Button } from '../Buttons/Button'
-import { NewComment } from '../NewComment/NewComment'
-import { User } from '../User/User'
-import { Comment } from '../Comment/Comment'
-import PostCardCSS from "./PostCard.module.css"
+import React from "react";
+import { Button } from "../Buttons/Button";
+import { NewComment } from "../NewComment/NewComment";
+import { User } from "../User/User";
+import { Comment } from "../Comment/Comment";
+import PostCardCSS from "./PostCard.module.css";
+import { useSelector } from "react-redux";
 
-function PostCard() {
+function PostCard({ content, username, _id }) {
+
+  const {allUsers} = useSelector( state => state.users);
+  const currentUser = allUsers.find(user => user.username === username);
+  
   return (
     <div className={` d-flex ${PostCardCSS.container}`}>
-        <header className={`d-flex ${PostCardCSS.header}`}>
-            <User/>
-            <i className="bi bi-three-dots-vertical"></i>
-        </header>
-        <article>I am post content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quos dicta consectetur, exercitationem deleniti ut quasi voluptates quibusdam facere iure quidem quas enim vero amet fugit dolore distinctio minus sunt?
-        </article>
-        <Button/>
-        <NewComment/>
-        <Comment/>
+      <header className={`d-flex ${PostCardCSS.header}`}>
+        <User username={username} firstName={currentUser?.firstName} lastName={currentUser?.lastName} image={currentUser?.profilePic} />
+        <i className="bi bi-three-dots-vertical"></i>
+      </header>
+      <article>{content}</article>
+      <Button />
+      <NewComment />
+      {/* <Comment /> */}
     </div>
-  )
+  );
 }
 
-export {PostCard}
+export { PostCard };
