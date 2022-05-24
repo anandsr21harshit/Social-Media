1a877554f7392805ce927eb18fdb6c9642e7e98c
Harshit Anand <harshitanand97@gmail.com>
2022-05-24 12:04:41 +0530

    Added edit modal

--- src/frontend/components/PostCard/PostCard.jsx
+++ src/frontend/components/PostCard/PostCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Button } from "../Buttons/Button";
 import { NewComment } from "../NewComment/NewComment";
 import { User } from "../User/User";
@@ -8,6 +8,7 @@ import { useSelector } from "react-redux";
 
 function PostCard({ content, username, _id }) {
 
+  const [editModal, setEditModal] = useState(false);
   const {allUsers} = useSelector( state => state.users);
   const currentUser = allUsers.find(user => user.username === username);
   
@@ -15,8 +16,14 @@ function PostCard({ content, username, _id }) {
     <div className={` d-flex ${PostCardCSS.container}`}>
       <header className={`d-flex ${PostCardCSS.header}`}>
         <User username={username} firstName={currentUser?.firstName} lastName={currentUser?.lastName} image={currentUser?.profilePic} />
-        <i className="bi bi-three-dots-vertical"></i>
+        <i className="bi bi-three-dots-vertical" onClick={()=> setEditModal(!editModal)}></i>
       </header>
+      {editModal && (
+          <ul className={PostCardCSS.edit_modal}>
+            <li><i className="bi bi-pencil-square"></i>Edit</li>
+            <li><i className="bi bi-trash-fill"></i>Delete</li>
+          </ul>
+        )}
       <article>{content}</article>
       <Button />
       <NewComment />
