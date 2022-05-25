1206617af2735939f4e2ceb8749ded41b4b9444d
Harshit Anand <harshitanand97@gmail.com>
2022-05-25 20:36:33 +0530

    Update PostCard.jsx

--- src/frontend/components/PostCard/PostCard.jsx
+++ src/frontend/components/PostCard/PostCard.jsx
@@ -4,24 +4,39 @@ import { NewComment } from "../NewComment/NewComment";
 import { User } from "../User/User";
 import { Comment } from "../Comment/Comment";
 import PostCardCSS from "./PostCard.module.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { openModal } from "../../features/post/postModalSlice";
+import { deleteUserPost } from "../../features/post/postSlice";
 
-function PostCard({ content, username, _id }) {
+function PostCard({post}) {
 
+  const { content, username, _id } = post;
+  const dispatch = useDispatch();
   const [editModal, setEditModal] = useState(false);
   const {allUsers} = useSelector( state => state.users);
+  const loggedInUser = JSON.parse(localStorage.getItem("loginCred")).user;
   const currentUser = allUsers.find(user => user.username === username);
+
+  const editHandler = () => {
+    dispatch(openModal(post));
+    setEditModal(false);
+  }
+
+  const deleteHandler = (id) => {
+    dispatch(deleteUserPost(id));
+    setEditModal(false);
+  }
   
   return (
     <div className={` d-flex ${PostCardCSS.container}`}>
       <header className={`d-flex ${PostCardCSS.header}`}>
         <User username={username} firstName={currentUser?.firstName} lastName={currentUser?.lastName} image={currentUser?.profilePic} />
-        <i className="bi bi-three-dots-vertical" onClick={()=> setEditModal(!editModal)}></i>
+        {username === loggedInUser.username && <i className="bi bi-three-dots-vertical" onClick={()=> setEditModal(!editModal)}></i>}
       </header>
       {editModal && (
           <ul className={PostCardCSS.edit_modal}>
-            <li><i className="bi bi-pencil-square"></i>Edit</li>
-            <li><i className="bi bi-trash-fill"></i>Delete</li>
+            <li onClick={() => editHandler(_id)}><i className="bi bi-pencil-square"></i>Edit</li>
+            <li onClick={()=>deleteHandler(_id)}><i className="bi bi-trash-fill"></i>Delete</li>
           </ul>
         )}
       <article>{content}</article>
