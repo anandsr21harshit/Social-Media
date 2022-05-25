cb7fba3e3600963eca5dcde312ddd3d80ae2b349
Harshit Anand <harshitanand97@gmail.com>
2022-05-25 20:36:10 +0530

    Update: added users and modal reducer

--- src/frontend/app/store.js
+++ src/frontend/app/store.js
@@ -2,11 +2,13 @@ import {configureStore} from "@reduxjs/toolkit"
 import authReducer from "../features/auth/authSlice"
 import postReducer from "../features/post/postSlice"
 import userReducer from "../features/user/userSlice"
+import postModalReducer from "../features/post/postModalSlice"
 
 export const store = configureStore({
     reducer:{
         auth: authReducer,
         posts: postReducer,
-        users: userReducer
+        users: userReducer,
+        postModal: postModalReducer
     }
 })
