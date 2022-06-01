53ad28acd59c9eb3be126476b53c7722ab80fb89
Harshit Anand <harshitanand97@gmail.com>
2022-06-01 23:43:51 +0530

    Update: dynamic data

--- src/frontend/components/Comment/Comment.jsx
+++ src/frontend/components/Comment/Comment.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import CommentCSS from "./Comment.module.css"
 import { User } from '../User/User'
+import { useSelector } from 'react-redux'
 
-function Comment() {
+function Comment({commentUser}) {
+  const {allUsers} = useSelector(state => state.users);
+  const userWhoCommented = allUsers.filter(user => user.username === commentUser.username)[0];
+
+  const {username,firstName, lastName, profilePic} = userWhoCommented;
+  
   return (
     <div className={CommentCSS.container}>
-         <User/>
+         <User username={username} firstName={firstName} lastName={lastName} image={profilePic} flag={true}/>
         <div className={CommentCSS.comment}>
+          {commentUser.text}
         </div>
     </div>
   )
