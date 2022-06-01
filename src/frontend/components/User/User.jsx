15c01c440945c5cdd6fbf5b9a77f162563ed2c04
Harshit Anand <harshitanand97@gmail.com>
2022-06-01 23:44:18 +0530

    Update User.jsx

--- src/frontend/components/User/User.jsx
+++ src/frontend/components/User/User.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import UserCSS from "./User.module.css"
 import man from "../../assets/man.png"
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 
-function User({username, firstName, lastName, image}) {
+function User({username, firstName, lastName, image, flag}) {
  
   return (
     <div className={`d-flex ${UserCSS.container}`}>
       <img src={image} alt="User Icon" />
       <div>
       <h4>{firstName} {lastName}</h4>
-      <p className={UserCSS.username}>@{username}</p>
+      {!flag && <p className={UserCSS.username}>@{username}</p>}
       </div>
     </div>
   );
