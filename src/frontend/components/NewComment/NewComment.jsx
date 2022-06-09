abf6c2180a3098d07529c93b495ec10f8386e4ea
Harshit Anand <harshitanand97@gmail.com>
2022-06-09 23:22:45 +0530

    Update NewComment.jsx

--- src/frontend/components/NewComment/NewComment.jsx
+++ src/frontend/components/NewComment/NewComment.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import man from "../../assets/man.png";
 import arrow from "../../assets/right-arrow.png";
 import { addComment } from "../../features/post/postSlice";
@@ -16,9 +16,11 @@ function NewComment({post}) {
     setCommentData("");
   }
 
+  const {user} = useSelector(state => state.auth);
+
   return (
     <div className={`d-flex ${NewCommentCSS.container}`}>
-      <img src={man} alt="User Icon" />
+      <img src={user.profilePic} alt="User Icon" />
       <div className={NewCommentCSS.comment_input}>
         <textarea
           placeholder="Write your comment"
