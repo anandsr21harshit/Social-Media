9990bf93fea0d387492730a634b9d7d07ace8881
Harshit Anand <harshitanand97@gmail.com>
2022-06-09 23:22:36 +0530

    Update Profile.jsx

--- src/frontend/pages/profile/Profile.jsx
+++ src/frontend/pages/profile/Profile.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { PostCard } from '../../components/PostCard/PostCard'
 import { ProfileCard } from '../../components/ProfileCard/ProfileCard'
+import { getAllPosts } from '../../features/post/postSlice';
+import { getAllUsers } from '../../features/user/userSlice';
 import ProfileCSS from "./Profile.module.css"
 
 function Profile() {
+  const [feedPosts, setFeedPosts] = useState([]);
+  const {user} = useSelector(state => state.auth);
+  const {allPosts} = useSelector(state => state.posts);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllPosts());
+    dispatch(getAllUsers());
+  }, []);
+
+  useEffect(() => {
+    if (allPosts) {
+      setFeedPosts(
+        allPosts.filter(
+          (post) =>
+            post.username === user.username ||
+            user.following.find((person) => post.username === person.username)
+        )
+      );
+    }
+  },[user, allPosts]);
   return (
     <div className={`d-flex ${ProfileCSS.container}`}>
-        <ProfileCard/>
-        {/* <PostCard/>
-        <PostCard/> */}
+        <ProfileCard user={user}/>
+        {feedPosts.map(post => <PostCard post={post} key={post._id}/>)}
     </div>
   )
 }
